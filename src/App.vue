0ed9631a26f3b555df43d8a0cbaecb12b4518fec
Vlad-Moskalenko <vlad.moskalenko1993@gmail.com>
2023-09-26 07:16:05 +0300

    init: init quiz app

---
+++ src/App.vue
@@ -0,0 +1,10 @@
+<script>
+import HomeView from './pages/HomeView.vue';
+import QuizView from './pages/QuizView.vue';
+</script>
+<template>
+  <div>
+    <QuizView />
+  </div>
+</template>
+<style scoped></style>
